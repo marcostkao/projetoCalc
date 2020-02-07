98536bcdca017102f8214fd0b1e7ba8c71702e82
Treinamento <Treinamento@RD723900.raiadrogasil.com>
2020-02-07 09:48:05 -0300

    atualização final

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// Adiciona numero no visor
+let visor = document.getElementById("visor")
+function pegarNum(num){
+    if (typeof visor == null){
+        document.numG.visor.value= '';
+    }
+   
+}
